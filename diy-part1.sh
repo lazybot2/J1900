0eb97d77096c8af46a13274b211360e290bb79d7
lazybot2 <54000554+lazybot2@users.noreply.github.com>
2024-01-29 15:28:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,4 @@
 echo 'src-git passwalls https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
-echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
+
